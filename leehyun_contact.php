294951f54b934f85bf8c9f3c9a68be7ca0a759f2
hoyahoya0517 <103160535+hoyahoya0517@users.noreply.github.com>
2022-06-22 17:00:47 +0900

    Update leehyun_contact.php

--- leehyun_contact.php
+++ leehyun_contact.php
@@ -49,7 +49,7 @@
     </style>
   <body>
     <header id="header">
-        <h1><a href=index.php>LEEHYUN SYSTEM</a></h1>
+        <h1><a href=index.html>LEEHYUN SYSTEM</a></h1>
     </header>
     <div id="contact">
       <div id="location">
