2bbd08b904b8e7abf6db9b299f972ccdbeb3f3a4
hoyahoya0517 <103160535+hoyahoya0517@users.noreply.github.com>
2022-06-22 16:57:40 +0900

    Add files via upload

---
+++ leehyun_about.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>연습</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto+Slab:wght@900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&family=Poppins:wght@600&family=Roboto+Slab:wght@900&display=swap');
+      body {
+        background-color: white;
+      }
+      a {
+        text-decoration-line: none;
+      }
+      #header {
+        text-align: center;
+        padding-top: 20px;
+      }
+      #header h1 {
+         font-family: 'Poppins', sans-serif;
+         font-size: 50px;
+      }
+      #header a {
+        color:black;
+      }
+      .about {
+        position: absolute;
+        top : 30%;
+        left: 4%;
+        right: 5%;
+        margin-bottom: 30px;
+      }
+      .about #a1 p{
+        font-family: 'Poppins', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+      }
+      .about #a2 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+      }
+      .about #a3 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+      }
+      .about #a3 span{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        font-weight:900;
+      }
+      .about #a4 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-top: 90px;
+        margin-bottom: 90px;
+      }
+      .about #a5 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+        font-weight:700;
+      }
+      .about #a6 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+      }
+      .about #a7 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+      }
+      .about #a8 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-top: 90px;
+        margin-bottom: 90px;
+      }
+      .about #a9 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 25px;
+        margin-bottom: 30px;
+        font-weight:700;
+      }
+      .about #a10 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 20px;
+        margin-bottom: 30px;
+      }
+      .about #img1 {
+        margin-top: 120px;
+      }
+
+
+
+    </style>
+  <body>
+    <header id="header">
+      <h1><a href=index.php>LEEHYUN SYSTEM</a></h1>
+    </header>
+
+    <div class="about">
+
+      <div id="a1">
+        <p>GYEONGGI DO,KOREA 2011.</p>
+      </div>
+
+      <div id="a2">
+        <p>
+          이현시스템은 2011년에 설립한 상황실 구축 전문 업체입니다.
+          DELTA DLP CUBE 국내 총판으로 상황실 솔루션 및 기타 솔루션을 제공하는 전문회사이며, 상황/관제 시스템
+          관련 유지보수 업무를 담당하고 있습니다.
+        </p>
+      </div>
+      <div id="a3">
+        <p>
+          <span id="date">2011. 07 -</span> 이현시스템 법인 설립
+        </p>
+        <p>
+          <span id="date">2012. 01 -</span> Delta Cube 한국 총판 계약 – Delta Korea
+        </p>
+        <p>
+          <span id="date">2012. 04 -</span> 부산 교통정보센터 상황실 유지보수 계약
+        </p>
+        <p>
+          <span id="date">2012. 06 -</span> 본사 이전 (성남시 상대원동 우림라이온스밸리 5차 1512호)
+        </p>
+        <p>
+          <span id="date">2012. 12 -</span> 약 14건의 DELTA 완제품 및 엔진 구축 프로젝트 수행
+        </p>
+        <p>
+          <span id="date">2012. 12 -</span> 연 매출 25억 달성
+        </p>
+        <p>
+          <span id="date">2013. 03 -</span> 연구개발전담부서 설립 (한국산업기술진흥협회 인정)
+        </p>
+        <p>
+          <span id="date">2013. 05 -</span> 벤처인증
+        </p>
+        <p>
+          <span id="date">2013. 12 -</span> 연 매출 30억 달성
+        </p>
+        <p>
+          <span id="date">2014. 05 -</span> 기업부설연구소 인정서(한국산업기술진흥협회 인정)
+        </p>
+
+      </div>
+      <div id="a4">
+        <p>
+          ㅡ
+        </p>
+      </div>
+      <div id="a5">
+        <p>
+          주요업무
+        </p>
+      </div>
+      <div id="a6">
+        <p>
+          DELTA 완제품 및 엔진에 대한 국내 총판 업무,Polycom 화상회의 국내 영업 및 설치 업무,
+          DLP CUBE 및 화상회의 유지보수,삼성SDS 제품도 DELTA 엔진을 사용.
+        </p>
+      </div>
+      <div id="a7">
+        <p>
+          지자체 감시사업소방 교통정보관련분야 ITS 어린이생활안전 CCTV 무인시설보안감시 공항 경비보안사업
+          영상정보공유사업군 군과학화 감시체계사업 작업현장 상황감시
+        </p>
+      </div>
+      <div id="a8">
+        <p>
+          ㅡ
+        </p>
+      </div>
+      <div id="a9">
+        <p>
+          주요실적
+        </p>
+      </div>
+      <div id="a10">
+        <p><a id="sil1" style="cursor:pointer">DELTA DLP CUBE 실적</a></p>
+        <img id="sil1_img" style="display:none;" src="sil1.png">
+        <p></p>
+        <p><a id="sil2" style="cursor:pointer">유지보수 실적</a></p>
+        <img id="sil2_img" style="display:none;" src="sil2.png" width="477">
+        <p></p>
+        <p><a id="sul" style="cursor:pointer">DELTA DLP CUBE 설치 사례</a></p>
+        <img id="sul1_img" style="display:none;" src="sul1.png">
+        <p></p>
+        <img id="sul2_img" style="display:none;" src="sul2.png">
+        <p></p>
+        <img id="sul3_img" style="display:none;" src="sul3.png">
+        <p></p>
+        <img id="sul4_img" style="display:none;" src="sul4.png">
+        <p></p>
+        <img id="sul5_img" style="display:none;" src="sul5.png">
+        <p></p>
+      </div>
+      <div id="img1">
+        <p><img src="lee_logo.png" alt="" width="400"></p>
+        <p><img src="delta_logo.png" alt="" width="150"></p>
+      </div>
+    </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+       $(document).ready(function(){
+          $('#sil1').on("click", function(){
+             $('#sil1_img').toggle();
+          });
+       });
+       $(document).ready(function(){
+          $('#sil2').on("click", function(){
+             $('#sil2_img').toggle();
+          });
+       });
+       $(document).ready(function(){
+          $('#sul').on("click", function(){
+             $('#sul1_img').toggle();
+             $('#sul2_img').toggle();
+             $('#sul3_img').toggle();
+             $('#sul4_img').toggle();
+             $('#sul5_img').toggle();
+          });
+       });
+    </script>
+  </body>
+</html>
---
+++ leehyun_contact.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>연습</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto+Slab:wght@900&display=swap');
+      body {
+        background-color: black;
+      }
+      a {
+        text-decoration-line: none;
+      }
+      #header {
+        text-align: center;
+        padding-top: 20px;
+      }
+      #header h1 {
+         font-family: 'Poppins', sans-serif;
+         font-size: 50px;
+      }
+      #header a {
+        color:white;
+      }
+      #contact{
+        text-align: center;
+        position: absolute;
+        top: 60%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+      #contact #location p{
+        color:white;
+        font-family: 'Poppins', sans-serif;
+      }
+      #contact #company p{
+        color:white;
+        font-family: 'Poppins', sans-serif;
+      }
+      #contact #oh p{
+        color:white;
+        font-family: 'Poppins', sans-serif;
+      }
+      #contact #lee p{
+        color:white;
+        font-family: 'Poppins', sans-serif;
+      }
+    </style>
+  <body>
+    <header id="header">
+        <h1><a href=index.php>LEEHYUN SYSTEM</a></h1>
+    </header>
+    <div id="contact">
+      <div id="location">
+        <p>#F819, MISACENTUMBIZ, 45 JOJUNGDAE-RO, HANAM-SI, GYEONGGI-DO, KOREA</p>
+      </div>
+      <div id="company">
+        <p>TEL : +82-31-5175-3331</p>
+        <p>FAX : +82-31-5175-3331</p>
+      </div>
+      <div id="oh">
+        <br><br><p>-</p><br><br>
+      </div>
+      <div id="lee">
+        <p>DIRECTOR  :  JONGHYUN, LEE</p>
+        <p>TEL : +82-10-6698-6828</p>
+        <p>EMAIL : ljh6919@daum.net</p>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
